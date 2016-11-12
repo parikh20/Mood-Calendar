5cd68b60728d30d47df5785bf52998edd8c73686
Jaineek Parikh <jaineekparikh@yahoo.com>
2016-11-12 09:26:44 -0800

    intial commit

---
+++ app/src/main/java/moodcalendar/parikh20/com/moodcalendar/MainActivity.java
@@ -0,0 +1,13 @@
+package moodcalendar.parikh20.com.moodcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
